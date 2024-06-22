a23949f7e604c129c83c3096079d408a1f4d2300
prashantsingh019 <ps731278@gmail.com>
2024-06-22 22:29:15 +0530

    did some changes added some functionality using js

--- public/script.js
+++ public/script.js
@@ -1,43 +1,166 @@
-// main function 
-function main(){
-     let search = document.querySelector("#search").value;
-     
-     weatherInfo(search);
+// API key
+const api_key = "9c5d0ab14a89b09845ed53502be4862a";
+
+// elements
+let cityInput = document.querySelector("#getCity");
+const get_btn = document.querySelector(".btn-get");
+const locate_btn = document.querySelector(".btn-locate");
+let city_display = document.querySelector(".cityName");
+const country = document.querySelector("#country");
+const temp = document.querySelector(".temp");
+const icon = document.querySelector(".weather-icon");
+const weatherCondition = document.querySelector(".weather-condition");
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "April",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const timeD = document.querySelector(".time");
+const dateD = document.querySelector(".date");
+const dayD = document.querySelector(".day");
+
+get_btn.addEventListener("click", () => {
+  findCoords(cityInput.value);
+  cityInput.value = "";
+});
+/****************************/
+
+//find coordinate
+function findCoords(cityName) {
+  try {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${api_key}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        display(data);
+      });
+  } catch (error) {
+    console.log(`Error while fetching ${error}`);
+  }
 }
-// default location
-weatherInfo("New Delhi");
 
-// cc0c50e64405d2233dd1a807f0e13724
-
-// fetch API using Async / Await
-
-async function weatherInfo(location){
-    try{
-      let url = await fetch(`http://api.weatherapi.com/v1/current.json?key=440ffce9deac4ee79ea20630241506&q=${location}`);
-      let data = await url.json();
-      insertData(data);
-      console.log(data);
-    }catch(error){
-       console.log(error);
+// fetching weather data
+async function fetchApi(data) {
+  try {
+    await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${data[0].lat}&lon=${data[0].lon}&exclude=hourly,minutely&units=metric&appid=${api_key}`
+    )
+      .then((res) => res.json())
+      .then((data) => weatherdisplay(data));
+  } catch (error) {
+    console.log(`Error while fetching API data:${error}`);
+  }
+}
+// displaying name
+function display(data) {
+  const country = data[0].country;
+  const state = data[0].state;
+  var locationE = `${data[0].name},${state},${country}`;
+  city_display.innerHTML = locationE;
+  fetchApi(data);
+}
+// displaying weatherData
+function weatherdisplay(data) {
+  console.log(data);
+  data.daily.forEach((element, index) => {
+    if (index == 0) {
+      return;
     }
-    
+  });
+  temp.innerHTML = `${data.current.temp} &deg;C`;
+  icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png" alt="weatherIcon">`;
+  weatherCondition.innerHTML = data.current.weather[0].description;
+  document.querySelector(".value1").innerHTML = `${data.current.pressure} hPa`;
+  document.querySelector(".value2").innerHTML = `${data.current.humidity}%`;
+  document.querySelector(
+    ".value3"
+  ).innerHTML = `${data.current.wind_speed} km/h`;
+  document.querySelector(
+    ".value4"
+  ).innerHTML = `${data.current.feels_like}&deg;C`;
+  document.querySelector(".time-zone").innerHTML = data.timezone;
+}
+// time function
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursin12 = hour >= 13 ? hour % 12 : hour;
+  const minutes = time.getMinutes();
+  if (minutes < 10) {
+    zero = 0;
+  } else {
+    zero = "";
+  }
+  const ampm = hour >= 12 ? "PM" : "AM";
+  timeD.innerHTML = `<span class="text-red-600">${hoursin12}</span>:${zero} ${minutes}<span id="am-pm"> ${ampm}</span>`;
+  //timeD.innerHTML = hoursin12 + ':' + zero + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
+  dateD.innerHTML = date + " " + months[month];
+  dayD.innerHTML = days[day];
+}, 1000);
+// find current location
+locate_btn.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition((success) => {
+    reverseFetch(success.coords.latitude, success.coords.longitude);
+  });
+});
+
+// location using reverse
+async function reverseFetch(latitude, longitude) {
+  try {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${api_key}`
+    )
+      .then((res) => res.json())
+      .then((result) => display(result));
+  } catch (error) {
+    console.log("something went wrong" + error);
+  }
 }
 
-// insert data 
-function insertData(weatherData){
-  const location = weatherData.location.name;
-  const country = weatherData.location.country;
-  const temperatureCelsius = weatherData.current.temp_c;
-  const humidity = weatherData.current.humidity;
-  const windSpeedKph = weatherData.current.wind_kph;
-  const conditionText = weatherData.current.condition.text;
-  const iconSet = weatherData.current.condition.icon;
-  // some insertion
-  document.querySelector(".cityName").innerHTML = `${location},`;
-  document.querySelector(".country").innerHTML = `${country}`;
-  document.querySelector(".temp-value").innerHTML = `${temperatureCelsius}`;
-  document.querySelector(".country").innerHTML = `${country}`;
-  document.querySelector(".country").innerHTML = `${country}`;
-  document.querySelector(".icon").innerHTML = `<img src = "https:${iconSet}" class = "w-3 inline-block">`;
-  document.querySelector(".condition").innerHTML = conditionText;
+// card creation
+function cardCreate() {
+  const cardContainer = document.querySelector(".container");
+  const card = document.createElement("div");
+  card.classList.add(
+    "card",
+    "border-2",
+    "border-gray-300",
+    "p-5",
+    "rounded-lg",
+    "bg-gray-500"
+  );
+  card.innerHTML = `
+    <div class="daynday text-lg font-bold">Sunday,23 June</div>
+   <div class="icon">ICON</div>
+   <div class="temp text-5xl">37&deg;C</div>
+  <div class="windspeed">windspeed: 10 KM/hr</div>
+    `;
+  cardContainer.appendChild(card);
 }
+
+// default City
+findCoords("New Delhi");
+
